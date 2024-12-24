69fdbda1249714a77d6cf80571f7c435dec2dee8
nepia0318 <49799401+nepia0318@users.noreply.github.com>
2024-12-25 03:39:51 +0900

    Add 3k1 search

--- src/main/event.py
+++ src/main/event.py
@@ -1,23 +1,32 @@
-import requests
+import os
+import re
 import discord
+from dotenv import load_dotenv
 from logging import getLogger
+
+from .search import getSearchResponse
+
+load_dotenv()
+word = os.getenv("3K1_QUERY")
 
 async def search(ctx):
     logger = getLogger(__name__)
 
+    query = word
     try:
-        embedMsg = discord.Embed(
-            title='hoge',
-            description=f'\
-                fuga: {'mannsyuu3923'}',
-            color=discord.Colour.green()
-        )
+        results = getSearchResponse(query)
+        await ctx.send(f'まん３に関する検索結果は{len(results)}件でした')
+        for result in results:
+            snippet = re.findall('(?<=\().+?(?=\))', result["snippet"])
+            embedMsg = discord.Embed(
+                title=result["title"],
+                url=result["link"],
+                description=f'\
+                    {result["snippet"]}',
+                color=discord.Colour.green()
+            )
 
-        await ctx.send(embed=embedMsg)
-
-    except requests.RequestException as e:
-        logger.info(f"Error: {e}")
-        await ctx.send('取得に失敗しました')
+            await ctx.send(embed=embedMsg)
 
     except Exception as e:
         logger.error(f"Error: {e}")
