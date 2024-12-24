5e9119bdf67a49e0355a1f51c31f3fd106a4fb1e
nepia0318 <49799401+nepia0318@users.noreply.github.com>
2024-12-25 00:22:17 +0900

    Initial commit

---
+++ src/main/event.py
@@ -0,0 +1,24 @@
+import requests
+import discord
+from logging import getLogger
+
+async def search(ctx):
+    logger = getLogger(__name__)
+
+    try:
+        embedMsg = discord.Embed(
+            title='hoge',
+            description=f'\
+                fuga: {'poyo'}',
+            color=discord.Colour.green()
+        )
+
+        await ctx.send(embed=embedMsg)
+
+    except requests.RequestException as e:
+        logger.info(f"Error: {e}")
+        await ctx.send('取得に失敗しました')
+
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        await ctx.send('取得に失敗しました')
